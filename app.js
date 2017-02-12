e634de5948a1605d72531b7edc6d6276a2181397
Сергей Тян <styan@sanscrit.kz>
2017-02-12 17:00:19 +0600

    add help message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const options = {
     polling: true
 };
 const bot = new TelegramBot(TOKEN, options);
-
+const helpMessage = "This is inline bot, just start typing @ArteezyBot in any other chat and you will see list of commands. If it's not working, visit https://arteezy-bot.herokuapp.com/ to wake him up.";
 const data = JSON.parse(fs.readFileSync('rtz_files.json', 'utf8'));
 
 bot.on('inline_query', function onCallbackQuery(callbackQuery) {
@@ -25,6 +25,13 @@ bot.on('inline_query', function onCallbackQuery(callbackQuery) {
     bot.answerInlineQuery(id, results);
 });
 
+function help(msg) {
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, helpMessage);
+}
+
+bot.onText(/\/start/, help);
+bot.onText(/\/help/, help);
 
 var express = require('express');
 var app = express();
@@ -34,6 +41,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){
     res.send('Thanks for waking me up! Arteezy bot here');
 });
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
